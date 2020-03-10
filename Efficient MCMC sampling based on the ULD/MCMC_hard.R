cb8a3d8511083530d580eb216f49c6dd15089d4e
Sharat Bhat <iamsharatbhat@gmail.com>
2020-03-11 00:48:51 +0530

    ULD Hard task included

---
+++ Efficient MCMC sampling based on the ULD/MCMC_hard.R
@@ -0,0 +1,99 @@
+library(volesti)
+library(ggplot2)
+library(geometry)
+
+num_dim = 200
+# step_size = 1
+error = 0.001
+
+# weight_matrix = matrix(, nrow = num_dim, ncol = 3)
+weight_matrix_vector = vector(mode = "numeric", length = 2*num_dim+1)
+weight_matrix_gradient_vector = vector(mode = "numeric", length = 2*num_dim)
+
+strong_convex_create <- function()
+{
+	weight_matrix_vector[1] <<- 50*runif(1)+50
+	for(i in 1:num_dim)
+	{
+		weight_matrix_vector[i+1] <<- 200000*runif(1) #- 100000
+		weight_matrix_gradient_vector[i] <<- weight_matrix_vector[i+1]
+	}
+	for(i in 1:num_dim)
+	{
+		weight_matrix_vector[i+num_dim+1] <<- runif(1)
+		weight_matrix_gradient_vector[i+num_dim] <<- 2*weight_matrix_vector[i+num_dim+1]
+	}
+	cat("Weight_matrix_vector: \n", weight_matrix_vector, "\n\n")
+	cat("Weight_matrix_gradient_vector: \n", weight_matrix_gradient_vector, "\n\n")
+	extremum = vector(mode="numeric", length = num_dim)
+}
+
+strong_convex_evaluate <- function(point_vector)
+{
+	ans = 0
+	point_matrix_vector <- vector(mode = "numeric", length = 2*num_dim+1)
+	point_matrix_vector[1] = 1
+	for(i in 2:num_dim+1)
+	{
+		point_matrix_vector[i] = point_vector[i-1]
+	}
+	for(i in num_dim+1:2*num_dim+1)
+	{
+		point_matrix_vector[i] = point_vector[i-1-num_dim]**2
+	}
+	for(i in 1:2*num_dim+1)
+	{
+		ans = ans + point_matrix_vector[i]*weight_matrix_vector[i]
+	}
+	return(ans)
+}
+
+strong_convex_gradient <- function(point_vector)
+{
+
+	point_matrix_vector = vector(mode = "numeric", length = num_dim)
+	for(i in 1:num_dim)
+	{
+		point_matrix_vector[i] = point_vector[i]*weight_matrix_gradient_vector[num_dim+i] + weight_matrix_gradient_vector[i]
+	}
+	ans = matrix(point_matrix_vector, nrow=1)
+
+	return(ans)
+}
+
+bb_gradient_descent <- function()
+{
+	previous_vector = rep(1, num_dim)
+	previous_vector_matrix = matrix(previous_vector, nrow = 1)
+	init_step = 0.001
+	current_vector_matrix = previous_vector_matrix - init_step*strong_convex_gradient(previous_vector)
+	flag = TRUE
+	current_vector = as.vector(current_vector_matrix)
+	previous_vector = as.vector(previous_vector_matrix)
+	num_steps = 0
+	while(flag == TRUE)
+	{
+		y_vector_matrix = strong_convex_gradient(current_vector) - strong_convex_gradient(previous_vector)
+		s_vector_matrix = strong_convex_evaluate(current_vector) - strong_convex_evaluate(previous_vector)
+		alpha = as.numeric(s_vector_matrix*t(current_vector - previous_vector))/as.numeric(s_vector_matrix*t(y_vector_matrix))
+		previous_vector_matrix = current_vector_matrix
+		current_vector_matrix = current_vector_matrix - alpha*strong_convex_gradient(current_vector)
+		current_vector = as.vector(current_vector_matrix)
+		previous_vector = as.vector(previous_vector_matrix)
+		flag = FALSE
+		for(i in 1:num_dim)
+		{
+			if(abs(current_vector[i] - previous_vector[i]) > error)
+			{
+				flag = TRUE
+			}
+		}
+		num_steps = num_steps+1
+	}	
+	cat("Number of Steps: ", num_steps,"\n\n")
+	return (current_vector)
+}
+
+strong_convex_create()
+maxima = bb_gradient_descent()
+cat("Maxima = ", maxima, "\n\n")
--- Efficient MCMC sampling based on the ULD/MCMC_medium.R
+++ Efficient MCMC sampling based on the ULD/MCMC_medium.R
@@ -2,14 +2,13 @@ library(volesti)
 library(ggplot2)
 library(geometry)
 
-num_dim = 40
+num_dim = 100
 error = 0.01
 step_size = 1
 num_points = 400
 
 distance <- function(point1, point2)
 {
-	# n = length(point1)
 	ans = 0
 	for(i in 1:num_dim)
 	{
@@ -21,7 +20,6 @@ distance <- function(point1, point2)
 
 strong_convex <- function(point_vector)
 {
-	n = length(point_vector)
 	ans = 0
 	for (coordinate in point_vector)
 	{
@@ -40,17 +38,6 @@ prob_density <- function(point_vector)
 component_of_walk <- function(cumu_freq, value)
 {
 	n = length(cumu_freq)
-	# cat(n, is.boolean(cumu_freq[0] > value),"\n")
-	# if ( is.null(cumu_freq[0] > value) ) { print("is null")}
-	# cat("ok\n")
-	# if(cumu_freq[0] > value)
-	# {
-	# 	return(-1)
-	# }
-	# else if(cumu_freq[n-1] < value)
-	# {
-	# 	return(1)
-	# }
 	begin = 1
 	end = n
 	mid = as.integer((begin+end)/2)
@@ -73,13 +60,11 @@ random_point <- function(start_point)
 {
 	n = num_dim - 1
 	num_parts = as.integer(1/error)
-	# next_point = start_point
 	cumu_freq = vector(mode="numeric", length = 2*num_parts)
 	radius = step_size
 	for (j in 1:(num_dim-2))
 	{
 		cumu = 0
-		# j=0
 		for(i in 1:num_parts)
 		{
 			temp = start_point
@@ -87,11 +72,9 @@ random_point <- function(start_point)
 			index = (n-1)/2
 			temp[j] = temp[j] + r
 			delta_freq = error*((1-r*r)**index)*prob_density(temp)
-			# cat(delta_freq, n, (1-r*r), index, (1-r*r)**index,"\n")
 			cumu = cumu + delta_freq
 			cumu_freq[i] = cumu		
 		}
-		# cat(cumu,"\n")
 		for(i in 1:num_parts)
 		{
 			temp = start_point
@@ -99,22 +82,15 @@ random_point <- function(start_point)
 			index = (n-1)/2
 			temp[j] = temp[j] + r
 			delta_freq = error*((1-r*r)**index)*prob_density(temp)
-			# cat(delta_freq,"\t")
 			cumu = cumu + delta_freq
 			cumu_freq[num_parts + i] = cumu
 		}
-		# cat("\n")
-		# cat(cumu_freq,"\n")
 		random = runif(1) * cumu
-		# cat(random, "\n")
 		x = radius*component_of_walk(cumu_freq, random)
 		start_point[j] = start_point[j] + x
-		# cat(start_point, "\n")
 		radius = (radius**2 - x**2)**0.5
-		# cat("x = ", x, "radius = ", radius, "\n")
 	}
 	theta = 2*pi*runif(1)
-	# cat(pi,"theta = ", theta, "\n")
 	start_point[num_dim-1] = start_point[num_dim-1] + radius*cos(theta)
 	start_point[num_dim] = start_point[num_dim] + radius*sin(theta)
 	return(start_point)
@@ -134,17 +110,12 @@ ball_walk <-function(start_point)
 	return(random_walk_points)
 }
 
-# prompt <- "Enter the point_vector:"
-# vec <- as.numeric(strsplit(readline(prompt), " ")[[1]])
-# vec = readline(prompt="Enter point vector: ")
-# vec = as.integer(vec)
+# Generating a random vector as Starting Point (as of now)
 vec = vector("numeric", num_dim)
 for(i in 1:num_dim)
 {
 	vec[i] = runif(1)
-	# vec[i] = 0
 }
-# cat(vec, strong_convex(vec), prob_density(vec), "\n")
 
 random_walk_points = ball_walk(vec)
 displacement = distance(random_walk_points[[1]], random_walk_points[[num_points]])
@@ -160,10 +131,6 @@ while(i2 == i1)
 {
 	i2 = as.integer(num_dim*runif(1))+1
 }
-# x = random_walk_points[i1,]
-# y = random_walk_points[i2,]
-# i1 = num_dim-1
-# i2 = num_dim
 cat(i1, i2, "\n")
 x = vector("numeric", num_points)
 for(i in 1:num_points)
@@ -175,8 +142,6 @@ for(i in 1:num_points)
 {
 	y[i] = random_walk_points[[i]][i2]
 }
-# cat(x,"\n")
-# cat(y,"\n")
 
 # Plotting them in a graph
 heading = paste("Random Ball Walk\nStep Size = ", step_size," Number of Dimensions = ", num_dim, "Number of Points = ", num_points)
