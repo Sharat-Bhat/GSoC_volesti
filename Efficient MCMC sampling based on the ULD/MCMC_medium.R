00927038e03dafba1c6459ef80530a6f0ac231d4
Sharat-Bhat <iamsharatbhat@gmail.com>
2020-03-23 18:56:21 +0530

    Code Shortened

--- Efficient MCMC sampling based on the ULD/MCMC_medium.R
+++ Efficient MCMC sampling based on the ULD/MCMC_medium.R
@@ -3,24 +3,28 @@ library(ggplot2)
 library(geometry)
 
 # Number of dimensions of the space
-num_dim = 3
+num_dim = 4
 step_size = 0.1
-num_points = 400000
+num_points = 100000
+num_samples = 10000
 
 # Vector containing the corresponding coefficients of the evaluate function
 weight_matrix_vector = vector(mode = "numeric", length = 2*num_dim+1)
 
+# Coordinate Axes
+p = num_dim-2
+q = num_dim
+
 distance <- function(point1, point2)
 {
-	ans = 0
-	for(i in 1:num_dim)
-	{
-		ans = ans + (point1[i] - point2[i])**2
-	}
+	diff = point2 - point1
+	ans = sum(diff*diff)
 	ans = sqrt(ans)
 	return(ans)
 }
 
+# For Testing Purpose only
+# Randomly creates a strong convex function
 strong_convex_create <- function()
 {
 	weight_matrix_vector[1] <<- 100*runif(1)+100
@@ -36,18 +40,18 @@ strong_convex_create <- function()
 	}
 	# weight_matrix_vector[3] <<- 0
 	# weight_matrix_vector[num_dim+3] <<- 0
-	weight_matrix_vector[2] <<- 40
+	# weight_matrix_vector[2] <<- 0
 	# weight_matrix_vector[num_dim+2] <<- 0
 	cat("Weight_matrix_vector: \n", weight_matrix_vector, "\n\n")
-	cat("\nMean = ",-weight_matrix_vector[2]/(2*weight_matrix_vector[num_dim+2]), -weight_matrix_vector[num_dim+1]/(2*weight_matrix_vector[2*num_dim+1]), "\n")
-	cat("Standard Deviation = ", 1/sqrt(2*weight_matrix_vector[num_dim+2]), 1/sqrt(2*weight_matrix_vector[num_dim+4]),"\n")
+	cat("\nMean = ",-weight_matrix_vector[p+1]/(2*weight_matrix_vector[num_dim+p+1]), -weight_matrix_vector[q+1]/(2*weight_matrix_vector[num_dim+q+1]), "\n")
+	cat("Standard Deviation = ", 1/sqrt(2*weight_matrix_vector[num_dim+p+1]), 1/sqrt(2*weight_matrix_vector[num_dim+q+1]),"\n")
 }
 
 # Evaluates the value of the function at a particular point
 strong_convex_evaluate <- function(point_vector)
 {
 	ans = 0
-	point_matrix_vector <- vector(mode = "numeric", length = 2*num_dim+1)
+	point_matrix_vector = vector(mode = "numeric", length = 2*num_dim+1)
 	point_matrix_vector[1] = 1
 	for(i in 1:num_dim)
 	{
@@ -61,6 +65,7 @@ strong_convex_evaluate <- function(point_vector)
 	{
 		ans = ans + point_matrix_vector[i]*weight_matrix_vector[i]
 	}
+	ans = ans / 10
 	return(ans)
 }
 
@@ -70,22 +75,13 @@ random_point <- function(start_point)
 	next_step = vector(mode="numeric", length = num_dim)
 	next_point = vector(mode="numeric", length = num_dim)
 	origin = rep(0, num_dim)
-	# cat(origin, "\n")
-	# for (i in 1: num_dim)
-	# {
-	# 	next_step[i] = rnorm(0, 1)
-	# }
 	next_step = rnorm(num_dim)
+	next_step = step_size / distance(origin, next_step) * next_step
 	# cat(next_step, "\n")
-	scaling = step_size / distance(origin, next_step)
-	for(i in 1: num_dim)
-	{
-		next_step[i] = scaling*next_step[i]
-		next_point[i] = start_point[i] + next_step[i]
-	}
+	next_point = start_point + next_step
 	transition = exp(-strong_convex_evaluate(next_point)+strong_convex_evaluate(start_point))
 	prob = runif(1)
-	# cat("\n", prob, transition, "\n")
+	# cat(prob, transition, "\n\n")
 	if (transition > prob)
 		return(next_point)
 	else
@@ -107,26 +103,22 @@ ball_walk <-function(start_point)
 }
 
 # Generating a random vector as Starting Point (as of now)
-vec = vector("numeric", num_dim)
-for(i in 1:num_dim)
-{
-	vec[i] = runif(1)
-	vec[i] = 0
-}
+vec = 10*runif(n = num_dim)
+vec = rep(-10, num_dim)
 strong_convex_create()
-cat("Start = ", vec[1], vec[3], "\n")
+cat("Start = ", vec[p], vec[q], "\n")
 random_walk_points = ball_walk(vec)
 displacement = distance(random_walk_points[[1]], random_walk_points[[num_points]])
-cat("End = ", random_walk_points[[num_points]][1], random_walk_points[[num_points]][num_dim], "\n")
+cat("End = ", random_walk_points[[num_points]][p], random_walk_points[[num_points]][q], "\n")
 x = vector("numeric", num_points)
 for(i in 1:num_points)
 {
-	x[i] = random_walk_points[[i]][1]
+	x[i] = random_walk_points[[i]][p]
 }
 y = vector("numeric", num_points)
 for(i in 1:num_points)
 {
-	y[i] = random_walk_points[[i]][num_dim]
+	y[i] = random_walk_points[[i]][q]
 }
 
 # Plotting them in a graph
@@ -134,3 +126,5 @@ heading = paste("Random Ball Walk\nStep Size = ", step_size," Number of Dimensio
 cat("Displacement = ", displacement, "\n")
 plot(x, y, type = "p", main = heading, col = "red", pch =".")
 # lines(x, y)
+# random_walk_points <- data.frame(random_walk_points)
+# ggplot(random_walk_points, aes(x = x, y = y)) + stat_density2d(aes(fill = ..density..), contour = F, geom = 'tile')
